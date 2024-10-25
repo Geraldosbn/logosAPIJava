87950ef0574ef1354b2338783fa9de49d557e4f3
geraldosbn <geraldosbn92@gmail.com>
2024-10-25 16:32:17 -0300

    first commit

---
+++ src/main/java/com/api/logosManager/LogosManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.logosManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogosManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogosManagerApplication.class, args);
+	}
+
+}
